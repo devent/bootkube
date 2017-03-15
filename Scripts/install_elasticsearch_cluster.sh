7e4efb7ab8f79c3c662a177fb3021daffc4490a0
Erwin Müller <erwin.mueller@nttdata.com>
2017-03-15 17:03:59 +0100

    Fix for loop.

--- Scripts/install_elasticsearch_cluster.sh
+++ Scripts/install_elasticsearch_cluster.sh
@@ -13,7 +13,9 @@ function changeWorkDir() {
 # Creates the elasticsearch configuration for the node.
 #
 function configure_elasticsearch() {
-  echo "Creating elasticsearch configuration for $EL_CLUSTER_NAME IP ${ELIPS[$1]}"
+  node=$1
+  ip=${ELIPS[$node]}
+  echo "Creating elasticsearch configuration for $EL_CLUSTER_NAME IP $ip"
 
   cat << EOF > /tmp/elasticsearch.yml
 network.host: 0.0.0.0
@@ -28,12 +30,12 @@ EOF
 # Install elasticsearch service on all three nodes
 #
 function install_elasticsearch_nodes() {
-  for (( node=0; node<${#ELIPS[@]}; node++ ));do
-  #for node in 0 1 2;do
-  echo "elasticsearch for node "$node
-  configure_etcd $node 
-  scp /tmp/elasticsearch.yml ${ELIPS[$node]}:/tmp/
-  ssh ${ELIPS[$node]} "\
+  $node=$1
+  ip=${ELIPS[$node]}
+  echo "elasticsearch for node $node"
+  configure_elasticsearch $node 
+  scp /tmp/elasticsearch.yml $ip:/tmp/
+  ssh $ip "\
   sudo mkdir /srv/el/$EL_CLUSTER_NAME/conf /srv/el/$EL_CLUSTER_NAME/data /srv/el/$EL_CLUSTER_NAME/templates; \
   sudo mv /tmp/elasticsearch.yml /srv/el/$EL_CLUSTER_NAME/conf/; \
   sudo docker run --name "$DATA_NAME" \
@@ -60,5 +62,6 @@ fi
 source $1 
 
 changeWorkDir
-configure_elasticsearch
-install_elasticsearch_nodes
+  for (( node=0; node<${#ELIPS[@]}; node++ ));do
+  install_elasticsearch_nodes $node
+done
